0af809074185511745fb40f6f537c60f902e208d
Idiocy502 <44687587+Idiocy502@users.noreply.github.com>
2019-05-21 09:51:56 +0800

    Update photo.js

--- js/photo.js
+++ js/photo.js
@@ -70,5 +70,6 @@
                     }
             setInterval(()=>{ /*按下鼠标开始准备拖拽*/
 		    console.log(111)
+		    var oWrap=document.querySelector('.wrap');
 						oWrap.style.transform='rotateX('+event2.x+'deg) rotateY('+event2.y+'deg)' 
 				},1)
